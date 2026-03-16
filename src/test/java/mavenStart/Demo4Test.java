8504fd66ad98ab34ba3d90dba22f1b30d6a34bf9
TANISTHA <tanisthapanda72@gmail.com>
2026-03-16 10:09:39 +0530

    New class added

--- src/test/java/mavenStart/Demo4Test.java
+++ src/test/java/mavenStart/Demo4Test.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
-public class Demo1Test {
+public class Demo4Test {
 
 	@Test
 	public void sample1Test(){
